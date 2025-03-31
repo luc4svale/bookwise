83f6ef170c2c310668eee6cab8bda4211ed71cce
Lucas Vale <alucasvalle@gmail.com>
2025-03-31 18:58:58 -0300

    style(header, search): standardize padding and spacing
    Updated styles for both header and search bar to standardize padding and spacing, improving visual consistency across components.

--- components/header.php
+++ components/header.php
@@ -2,7 +2,7 @@
     <!-- Navigation -->
     <nav class="max-w-screen-lg mx-auto md:flex md:items-center md:justify-between">
 
-      <div class="flex justify-between items-center p-3">
+      <div class="flex justify-between items-center px-6 py-3">
         <!-- Logo -->
         <a href="#" class="text-lime-600 font-bold text-2xl tracking-wide focus:outline-none">
           📚 Bookwise
@@ -13,7 +13,7 @@
       </div>
 
       <!-- Menu List -->
-      <ul id="menu-list" class="md:px-3 text-lg text-stone-400 flex flex-col gap-x-8 md:flex-row md:items-center z-[-1] md:z-auto absolute md:static -top-full opacity-0 md:opacity-100 transition-all ease-in-out duration-500">
+      <ul id="menu-list" class="md:px-6 text-lg text-stone-400 flex flex-col gap-x-8 md:flex-row md:items-center z-[-1] md:z-auto absolute md:static -top-full opacity-0 md:opacity-100 transition-all ease-in-out duration-500">
         <li>
           <a href="/explore" class="p-3 md:p-0 block hover:bg-stone-700 hover:text-stone-100 md:hover:bg-stone-800 hover:underline">
             Explorar
--- components/search-bar.php
+++ components/search-bar.php
@@ -1,5 +1,5 @@
-<section class="max-w-screen-lg mx-auto px-3 py-6 md:py-10">
-  <form class="flex w-full md:w-[37.5%] lg:w-[30%] ml-auto">
+<section class="max-w-screen-lg mx-auto p-6 md:py-10 grid grid-cols:1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+  <form class="flex md:col-start-2 lg:col-start-3">
     <input class="bg-stone-800 p-2 focus:outline-none border-l border-y rounded-l-md border-stone-400 block text-md text-stone-400 grow" type="text" name="search" id="search">
     <button class="flex bg-stone-400 text-stone-800 justify-center items-center text-2xl p-2 rounded-r-md cursor-pointer" type="submit">
       <i class="ph-bold ph-magnifying-glass"></i>
